86998e251308440bd22213d243afe0910052584c
Matt Locklin <mattlocklin@live.co.uk>
2019-05-28 07:33:33 +0100

    Setup Calculator and made static NumberButton Component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+    return (
+        <div>
+            <input class='number-button'
+                type="button"
+                value={props.value} />
+        </div>
+    )
+}
+
+export default NumberButton
